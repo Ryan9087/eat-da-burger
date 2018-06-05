67e086d4599d309f9167f1fa6cc940109ec3974a
Ryan McGeeney <ryanmcgeeney@yahoo.com>
2018-06-05 17:32:22 -0500

    001

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quadruple bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper JAcl Mushroom', true);
